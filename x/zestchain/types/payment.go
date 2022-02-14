5f227ef308ea8d482ec1952891f5d28d45fcead8
root <root@ip-172-31-23-98.eu-west-1.compute.internal>
2022-02-14 20:08:22 +0000

    payment wip1

---
+++ x/zestchain/types/payment.go
@@ -0,0 +1,90 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	xauthsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+func Pay(amount float32, recip string) {
+
+	txBuilder := client.TxConfig.NewTxBuilder()
+	msg := banktypes.NewMsgSend("cosmos1tmq820gl5ty3cnf6aa9d82g04qv4c6389tcg4v", recip, types.NewCoins(types.NewInt64Coin("ZEST", amount)))
+	err := txBuilder.SetMsgs(msg)
+	if err != nil {
+		return err
+	}
+
+	txBuilder.SetGasLimit(0)
+	txBuilder.SetFeeAmount(0)
+	txBuilder.SetMemo("Ad reward")
+	txBuilder.SetTimeoutHeight(0)
+
+	privs := []cryptotypes.PrivKey{"b25bb748165b6a78ff93bedac2e824697edbe98f8688a1f6b04ae6090f5afcad"}
+	accNums := []uint64{1} // The accounts' account numbers
+	accSeqs := []uint64{0} // The accounts' sequence numbers
+
+	var sigsV2 []signing.SignatureV2
+	for i, priv := range privs {
+		sigV2 := signing.SignatureV2{
+			PubKey: priv.PubKey(),
+			Data: &signing.SingleSignatureData{
+				SignMode:  client.TxConfig.SignModeHandler().DefaultMode(),
+				Signature: nil,
+			},
+			Sequence: accSeqs[i],
+		}
+
+		sigsV2 = append(sigsV2, sigV2)
+	}
+	err := txBuilder.SetSignatures(sigsV2...)
+	if err != nil {
+		return err
+	}
+
+	sigsV2 = []signing.SignatureV2{}
+	for i, priv := range privs {
+		signerData := xauthsigning.SignerData{
+			ChainID:       "zestchain",
+			AccountNumber: accNums[i],
+			Sequence:      accSeqs[i],
+		}
+		sigV2, err := tx.SignWithPrivKey(
+			client.TxConfig.SignModeHandler().DefaultMode(), signerData,
+			txBuilder, priv, client.TxConfig, accSeqs[i])
+		if err != nil {
+			return nil, err
+		}
+
+		sigsV2 = append(sigsV2, sigV2)
+	}
+	err = txBuilder.SetSignatures(sigsV2...)
+	if err != nil {
+		return err
+	}
+
+	txBytes, err := client.TxConfig.TxEncoder()(txBuilder.GetTx())
+	if err != nil {
+		return err
+	}
+
+	txJSONBytes, err := client.TxConfig.TxJSONEncoder()(txBuilder.GetTx())
+	if err != nil {
+		return err
+	}
+	txJSON := string(txJSONBytes)
+}
+
+func (ad *Ad) PayView(recip sdk.AccAddress) {
+	recipient := sdk.AccAddressFromBech32(recip)
+	Pay(1, recipient)
+}
+
+func (ad *Ad) PayClick(recip sdk.AccAddress) {
+	recipient := sdk.AccAddressFromBech32(recip)
+        Pay(9, recipient)
+}
