af6db1dc229b12b3c14e51278d89b225b46c441e
root <root@ip-172-31-23-98.eu-west-1.compute.internal>
2022-02-15 16:42:28 +0000

    CreateAd Logic

--- x/zestchain/keeper/msg_server_create_ad.go
+++ x/zestchain/keeper/msg_server_create_ad.go
@@ -2,16 +2,42 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cytruslabs/zestchain/x/zestchain/types"
+	kk "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-func (k msgServer) CreateAd(goCtx context.Context, msg *types.MsgCreateAd) (*types.MsgCreateAdResponse, error) {
+func (k kk.msgServer) CreateAd(goCtx context.Context, msg *types.MsgCreateAd) (*types.MsgCreateAdResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	adCount, found := k.Keeper.GetAdCount(ctx)
+	if !found {
+		panic("AdCount not found")
+	}
+	newIndex := strconv.FormatUint(adCount.counter, 10)
+	ad := types.Ad{
+		Index:   newIndex,
+		Title:   msg.Title,
+		Pot:     msg.Pot,
+		Url:     msg.Url,
+		Msg:     msg.Msg,
+		Tags:    msg.Tags,
+		Prefs:   msg.Prefs,
+		Creator: msg.Creator,
+	}
+	err := ad.Validate()
+	if err != nil {
+		return nil, err
+	}
+	k.Keeper.SetAd(ctx, ad)
 
-	return &types.MsgCreateAdResponse{}, nil
+	adCount.counter++
+	k.Keeper.SetAdCount(ctx, adCount)
+
+	return &types.MsgCreateAdResponse{
+		IdValue: newIndex,
+	}, nil
 }
+
