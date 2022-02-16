69dfdbe16a8def987a3be7fc75f17b8cc31cd932
root <root@ip-172-31-23-98.eu-west-1.compute.internal>
2022-02-16 00:35:00 +0000

    Payment messages logic fix

--- x/zestchain/keeper/msg_server_create_ad.go
+++ x/zestchain/keeper/msg_server_create_ad.go
@@ -19,7 +19,7 @@ func (k msgServer) CreateAd(goCtx context.Context, msg *types.MsgCreateAd) (*typ
 	ad := types.Ad{
 		Index:   newIndex,
 		Title:   msg.Title,
-		Pot:     uint64(msg.Pot),
+		Pot:     strconv.ParseUint(msg.Pot, 10, 64),
 		Url:     msg.Url,
 		Msg:     msg.Msg,
 		Tags:    msg.Tags,
