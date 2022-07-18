b75567e9b58d6109061a1a5b2fcd763618a55874
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:52 +0100

    Web frontend fixes + updates

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '@/views/Index.vue'
 import CreatePromo from '@/views/CreatePromo.vue'
-import Relayers from '@/views/Relayers.vue'
 
 const routerHistory = createWebHistory()
 const routes = [
@@ -9,13 +8,15 @@ const routes = [
     path: '/',
     component: Index,
   },
-  { path: '/createPromo', component: CreatePromo },
-  { path: '/relayers', component: Relayers },
+  {
+    path: '/createPromo',
+    component: CreatePromo
+  }
 ]
 
 const router = createRouter({
   history: routerHistory,
-  routes,
+  routes
 })
 
 export default router
