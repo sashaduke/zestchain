44d84a7095c90097f38a3f9346ca4768a4dafa18
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:41 +0100

    Fixed frontend prefix issue

--- vue/src/views/CreatePromo.vue
+++ vue/src/views/CreatePromo.vue
@@ -38,7 +38,7 @@
         class="sp-input"
         ref="msg"
         placeholder="Cytrus passively earns you crypto while you browse - no sign up needed! Join today at cytrus.io"
-	v-on:focus="focused = true"
+  v-on:focus="focused = true"
         v-on:blur="focused = false"
       /><br><br>
       <button class="sp-button" type="submit">Submit</button>
@@ -53,55 +53,60 @@ import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { MsgCreatePromo } from "@/tx.js";
 import { Api } from "@/rest.js"
+import { stringToPath } from "@cosmjs/crypto";
 const type = [["/cytruslabs.zestchain.zestchain.MsgCreatePromo", MsgCreatePromo]];
 const registry = new Registry(type);
 const fee = {
-    amount: [],
-    gas: "200000"
+  amount: [],
+  gas: "200000"
 };
 const rpcEndpoint = "http://zestcha.in:26657";
 export default {
-    name: "CreatePromo",
-    mounted() {	
-	const store = this.$store.getters;
-	const initClient = async function() {
-	    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(store["common/wallet/getMnemonic"]);
-	    const [user] = await wallet.getAccounts();
-	    const client = await SigningStargateClient.connectWithSigner(
-		rpcEndpoint,
-		wallet,
-		{ registry: registry }
-	    );
-	    return {user: user, client: client};
-	};	
-	const createPromo = async function(promo) {
-	    const c = await initClient();
-	    const msg = {
-		typeUrl: type[0][0],
-		value: promo
-	    };
-	    const result = await c.client.signAndBroadcast(c.user.address, [msg], fee);
-	    return result;
-	};
-	const submitForm = function(event) {
-	    const promo = {
-		creator: store["common/wallet/address"],
-		title: title.value,
-		pot: parseInt(pot.value),
-		url: url.value,
-		msg: msg.value,
-		tags: '',
-		prefs: '',
-	    };
-            event.preventDefault();
-	    console.log(createPromo(promo));
-	};
-        const form = this.$refs.form;
-	const title = this.$refs.title;
-        const pot = this.$refs.pot;
-        const url = this.$refs.url;
-        const msg = this.$refs.msg;
-        form.addEventListener("submit", submitForm);
-    }
+  name: "CreatePromo",
+  mounted() {	
+    const store = this.$store.getters;
+    const initClient = async function() {
+      const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+        store["common/wallet/getMnemonic"],
+        {prefix: "cytrus"}
+      );
+      const [user] = await wallet.getAccounts();
+      const client = await SigningStargateClient.connectWithSigner(
+        rpcEndpoint,
+        wallet,
+        { registry: registry }
+      );
+      return {user: user, client: client};
+    };	
+    const createPromo = async function(promo) {
+      const c = await initClient();
+      const msg = {
+        typeUrl: type[0][0],
+        value: promo
+      };
+      const result = await c.client.signAndBroadcast(c.user.address, [msg], fee, "Created new promotion");
+      return result;
+    };
+    const submitForm = function(event) {
+      const promo = {
+        creator: store["common/wallet/address"],
+        title: title.value,
+        pot: parseInt(pot.value),
+        url: url.value,
+        msg: msg.value,
+        tags: '',
+        prefs: '',
+      };
+      event.preventDefault();
+      console.log(createPromo(promo));
+    };
+    const form = this.$refs.form;
+    const title = this.$refs.title;
+    const pot = this.$refs.pot;
+    const url = this.$refs.url;
+    const msg = this.$refs.msg;
+    form.addEventListener("submit", submitForm);
+  }
 };
 </script>
+
