775338d69f3b4677dd64aa4b7b7fb9a1f4cf411c
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:34 +0100

    Remove old vue implementation

--- vueold/src/store/generated/cytruslabs/zestchain/cytruslabs.zestchain.zestchain/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Params } from "./module/types/zestchain/params";
-import { Promo } from "./module/types/zestchain/promo";
-import { PromoCount } from "./module/types/zestchain/promo_count";
-export { Params, Promo, PromoCount };
-declare const _default;
-export default _default;
