e6170e2cfa2549e69e092a07d4362eda19b497a7
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:26 +0100

    Reward logic fix

--- x/zestchain/types/reward.go
+++ x/zestchain/types/reward.go
@@ -12,6 +12,7 @@ import (
 	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"google.golang.org/grpc"
 )
 
 func GetTxConfig() client.TxConfig {
@@ -23,7 +24,7 @@ func GetTxConfig() client.TxConfig {
 func Reward(amount int64, recip cosm.AccAddress) (string, error) {
 	txConfig := GetTxConfig()
 	txBuilder := txConfig.NewTxBuilder() //client.TxConfig.NewTxBuilder()
-	treasury := cosm.AccAddressFromBech32("cosmos19h39v0scqlyesn0mfh3ug33d7samzd59qxr6l0")
+	treasury, _ := cosm.AccAddressFromBech32("cosmos19h39v0scqlyesn0mfh3ug33d7samzd59qxr6l0")
 	msg := bank.NewMsgSend(treasury, recip, cosm.NewCoins(cosm.NewInt64Coin("ZEST", amount)))
 	err := txBuilder.SetMsgs(msg)
 	if err != nil {
@@ -33,14 +34,14 @@ func Reward(amount int64, recip cosm.AccAddress) (string, error) {
 	var fee cosm.Coins = make([]cosm.Coin, 1)
 	fee[0] = cosm.Coin{
 		Denom:  "ZEST",
-		Amount: 0,
+		Amount: cosm.NewInt(0),
 	}
 	txBuilder.SetFeeAmount(fee)
 	txBuilder.SetGasLimit(0)
 	txBuilder.SetTimeoutHeight(0)
 	txBuilder.SetMemo("Ad reward")
 
-	priv1 := secp256k1.GenPrivKeyFromSecret("insert mnemonic here")
+	priv1 := secp256k1.GenPrivKeyFromSecret([]byte("insert mnemonic here"))
 	privs := []crypto.PrivKey{priv1}
 	accNums := []uint64{1} // The accounts' account numbers
 	accSeqs := []uint64{0} // The accounts' sequence numbers
@@ -58,7 +59,7 @@ func Reward(amount int64, recip cosm.AccAddress) (string, error) {
 
 		sigsV2 = append(sigsV2, sigV2)
 	}
-	err = txBuilder.SetSignatures(sigsV2)
+	err = txBuilder.SetSignatures(sigsV2...)
 	if err != nil {
 		return "", err
 	}
@@ -79,7 +80,7 @@ func Reward(amount int64, recip cosm.AccAddress) (string, error) {
 
 		sigsV2 = append(sigsV2, sigV2)
 	}
-	err = txBuilder.SetSignatures(sigsV2)
+	err = txBuilder.SetSignatures(sigsV2...)
 	if err != nil {
 		return "", err
 	}
@@ -89,12 +90,26 @@ func Reward(amount int64, recip cosm.AccAddress) (string, error) {
 		return "", err
 	}
 
-	txJSONBytes, err := txConfig.TxJSONEncoder()(txBuilder.GetTx())
+	grpcConn, _ := grpc.Dial(
+		"127.0.0.1:9090",
+		grpc.WithInsecure(),
+	)
+	defer grpcConn.Close()
+
+	txClient := tx.NewServiceClient(grpcConn)
+	grpcRes, err := txClient.BroadcastTx(
+		context.Background(),
+		&tx.BroadcastTxRequest{
+			Mode:    tx.BroadcastMode_BROADCAST_MODE_SYNC,
+			TxBytes: txBytes,
+		},
+	)
 	if err != nil {
-		return "", err
+		return err
 	}
-	txJSON := string(txJSONBytes)
-	return txJSON, nil
+
+	fmt.Println(grpcRes.TxResponse.Code) // Should be '0' if the tx is successful
+	return nil
 }
 
 func (promo *Promo) ViewReward(recip cosm.AccAddress) {
