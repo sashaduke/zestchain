3dc87de8e2acad849d628099fb8f4dcb3011732c
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:36 +0100

    Removed dependency

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vueLib from '@starport/vue'
-import CreatePromo from '@cytruslabs/frontend'
+
 
 const app = createApp(App)
 app.config.globalProperties._depsLoaded = true
-app.use(store).use(router).use(vueLib).use(CreatePromo).mount('#app')
+app.use(store).use(router).use(vueLib).mount('#app')
