47364faaa28e282faa284464fc1df2a7f356b759
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:47 +0100

    Implemented form validation + error feedback

--- vue/src/views/CreatePromo.vue
+++ vue/src/views/CreatePromo.vue
@@ -9,10 +9,10 @@
       </div>
       <div class="sp-box sp-shadow">
         <span style="font-size: 1.28rem">Title</span>
-        <form ref="form" onsubmit="return false">     
+        <form ref="form" onsubmit="return false">    
           <input
             class="sp-input"
-            ref="title"
+            v-model="title"
             placeholder="Earn Crypto with Cytrus"
             v-on:focus="focused = true"
             v-on:blur="focused = false"
@@ -20,7 +20,7 @@
           <span style="font-size: 1.28rem">Budget in ZEST (1000 minimum)</span>
           <input
             class="sp-input"
-            ref="pot"
+            v-model="pot"
             placeholder="10000"
             v-on:focus="focused = true"
             v-on:blur="focused = false"
@@ -28,19 +28,25 @@
           <span style="font-size: 1.28rem">URL</span>
           <input
             class="sp-input"
-            ref="url"
+            v-model="url"
             placeholder="cytrus.io"
             v-on:focus="focused = true"
             v-on:blur="focused = false"
           /><br><br>
-          <span style="font-size: 1.28rem">Message</span>
+          <span style="font-size: 1.28rem">Message Text</span>
           <input
             class="sp-input"
-            ref="msg"
+            v-model="msg"
             placeholder="Cytrus passively earns you crypto while you browse - no sign up needed! Join today at cytrus.io"
             v-on:focus="focused = true"
             v-on:blur="focused = false"
-          /><br><br>
+          /><br>
+          <p v-if="errors.length">
+            <br><b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p><br>
           <button class="sp-button" type="submit">Submit</button>
         </form>
       </div>
@@ -61,6 +67,44 @@ const fee = {
 const rpcEndpoint = "http://zestcha.in:26657";
 export default {
   name: "CreatePromo",
+  data() {
+    return {
+      errors: [],
+      title: null,
+      pot: null,
+      url: null,
+      msg: null
+    }
+  },
+  methods: {
+    validateForm() {
+      if (this.title && this.pot >= 1000 && this.url && this.msg) {
+        return true;
+      }
+      this.errors = [];
+
+      if (!this.title) {
+        this.errors.push('Ad title cannot be blank.');
+      }
+      if (!this.pot || this.pot < 1000) {
+        this.errors.push('Ad budget needs to be a minimum of 1000 ZEST.');
+      }
+      if (!this.url) {
+        this.errors.push('Ad URL cannot be blank.');
+      }
+      if (!this.msg) {
+        this.errors.push('Ad message text cannot be blank.');
+      }
+      event.preventDefault();
+      
+      if (!this.errors) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+  },
   mounted() {	
     const store = this.$store.getters;
     const createPromo = async function(promo) {
@@ -78,28 +122,28 @@ export default {
         typeUrl: type[0][0],
         value: promo
       };
-      const result = await client.signAndBroadcast(user.address, [msg], fee, "Created new promotion");
+      const result = await client.signAndBroadcast(user.address, [msg], fee);
       return result;
     };
-    const submitForm = function(event) {
+    const submitForm = function() {
       const promo = {
         creator: store["common/wallet/address"],
-        title: title.value,
-        pot: parseInt(pot.value),
-        url: url.value,
-        msg: msg.value,
+        title: this.title.value,
+        pot: parseInt(this.pot.value),
+        url: this.url.value,
+        msg: this.msg.value,
         tags: '',
         prefs: '',
       };
-      event.preventDefault();
       console.log(createPromo(promo));
     };
+    const validateAndSubmit = () => {
+      if (this.validateForm()) {
+        submitForm();
+      }
+    }
     const form = this.$refs.form;
-    const title = this.$refs.title;
-    const pot = this.$refs.pot;
-    const url = this.$refs.url;
-    const msg = this.$refs.msg;
-    form.addEventListener("submit", submitForm);
+    this.form.addEventListener("submit", validateAndSubmit);
   }
 };
 </script>
