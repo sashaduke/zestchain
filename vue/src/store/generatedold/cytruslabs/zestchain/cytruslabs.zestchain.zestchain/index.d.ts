c2e15e47cca451317b3fa1f9818c22f098666ec2
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:34 +0100

    Implemented Scarcity / Burning

--- vue/src/store/generatedold/cytruslabs/zestchain/cytruslabs.zestchain.zestchain/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Ad } from "./module/types/zestchain/ad";
-import { AdCount } from "./module/types/zestchain/ad_count";
-import { Params } from "./module/types/zestchain/params";
-export { Ad, AdCount, Params };
-declare const _default;
-export default _default;
--- x/zestchain/keeper/msg_server_create_promo.go
+++ x/zestchain/keeper/msg_server_create_promo.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"math"
 	"strconv"
 
 	cosm "github.com/cosmos/cosmos-sdk/types"
@@ -23,8 +24,21 @@ func (k msgServer) CreatePromo(goCtx context.Context, msg *types.MsgCreatePromo)
 	if err != nil {
 		panic("Creator address not valid")
 	}
+
 	moduleAddr := cosm.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
-	err = k.bank.SendCoins(ctx, addr, moduleAddr, cosm.NewCoins(cosm.NewCoin("ZEST", cosm.NewInt(int64(msg.Pot)))))
+	burnAddr := cosm.AccAddress(crypto.AddressHash([]byte("burn")))
+
+	rem := int64(0)
+	if msg.Pot%2 == 1 {
+		rem = int64(1)
+	}
+	amt := int64(math.Floor(float64(msg.Pot / 2)))
+
+	err = k.bank.SendCoins(ctx, addr, moduleAddr, cosm.NewCoins(cosm.NewCoin("ZEST", cosm.NewInt(amt+rem))))
+	if err != nil {
+		return &types.MsgCreatePromoResponse{}, sdkerrors.Wrapf(err, types.ErrNotEnoughZEST.Error())
+	}
+	err = k.bank.SendCoins(ctx, addr, burnAddr, cosm.NewCoins(cosm.NewCoin("ZEST", cosm.NewInt(amt))))
 	if err != nil {
 		return &types.MsgCreatePromoResponse{}, sdkerrors.Wrapf(err, types.ErrNotEnoughZEST.Error())
 	}
