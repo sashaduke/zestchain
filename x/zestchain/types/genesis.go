17adb3ae2057a580b3e2003e381d04ebbaf32d10
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:06 +0100

    Payments WIP

--- x/zestchain/types/genesis.go
+++ x/zestchain/types/genesis.go
@@ -10,7 +10,7 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		AdCount: nil,
+		AdCount: uint64(0),
 		AdList:  []Ad{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
