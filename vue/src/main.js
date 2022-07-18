1dcdef7dd4a61ff8e6908a375049f2abbc5228cc
Sasha Duke <sasha.duke@qredo.com>
2022-07-18 16:40:21 +0100

    Update main.js

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-import vueLib from '@starport/vue'
+import vueLib from '@cytruslabs/vue'
 
 const app = createApp(App)
 app.config.globalProperties._depsLoaded = true
